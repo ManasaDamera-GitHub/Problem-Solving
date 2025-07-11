615af576cf3db17fe357ed15c8c0a3ebadfa4710
DameraManasa-GitHub <dameramanasa25@gmail.com>
2025-07-11 14:37:24 +0530

    ps

--- Strings/longestWord.py
+++ Strings/longestWord.py
@@ -13,3 +13,17 @@ if len(current)>len(longest):
 print(longest)
 
 # Approach2: # Using split() method to break the string into words and then finding the longest word.
+
+    # a="The quick brown fox jumps over the lazy dog".split()
+    # big=a[0]
+    # for i in range(1,len(a)):
+    #     if len(big)<=len(a[i]):
+    #         big=a[i]
+    # print(big)
+        
+    
+
+        
+
+
+        
